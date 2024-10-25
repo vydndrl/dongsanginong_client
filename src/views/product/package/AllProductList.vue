27c4af907e0a9939c981aed2c14f1be0291765ec
vydndrl <vydndrl1234@naver.com>
2024-10-25 18:01:59 +0900

    fix: 불필요한 console 제거

--- src/views/product/package/AllProductList.vue
+++ src/views/product/package/AllProductList.vue
@@ -165,11 +165,13 @@ export default {
             // { "src": "https://dongsanginong-bucket.s3.ap-northeast-2.amazonaws.com/Farm/f94275a5-35bc-4c2a-9ac4-4d6c69f2aa90%EC%8A%A4%ED%81%AC%EB%A6%B0%EC%83%B7%202024-10-22%20233639.png", "alt": "배너사진3", "link": "/event2" },
             // { "src": "https://dongsanginong-bucket.s3.ap-northeast-2.amazonaws.com/Farm/ca7a17bc-0a34-477e-bca5-9f80c539293a%EC%8A%A4%ED%81%AC%EB%A6%B0%EC%83%B7%202024-10-22%20233941.png", "alt": "배너사진4", "link": "/event2" },
         ];
+        console.log("1번")
         // Top 10 패키지 가져오기
         try {
+            console.log("2번")
             const topPackagesResponse = await axios.get(`${process.env.VUE_APP_API_BASE_URL}/product-service/product/no-auth/top10`);
             this.topPackageList = topPackagesResponse.data;
-            console.log(">>>>> topPackageList : ", this.topPackageList[0].discountId);
+            // console.log(">>>>> topPackageList : ", this.topPackageList[0].discountId);
 
             // 전체 패키지 리스트 가져오기
             let params = {
@@ -180,7 +182,7 @@ export default {
             }
             const packageListResponse = await axios.get(`${process.env.VUE_APP_API_BASE_URL}/product-service/product/no-auth`, { params });
             this.packageList = packageListResponse.data.content;
-            console.log(this.packageList)
+            console.log(">>>response: ", this.packageList)
             // sortOptionMap 설정
             this.sortOptionMap.set("최신순", "id,desc");
             this.sortOptionMap.set("판매량 순", "orderCount,desc");
