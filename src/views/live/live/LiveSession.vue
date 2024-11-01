fab68481ae685f71232822ef0c439eabd63a1027
H-Yeji <yeji980603@naver.com>
2024-11-01 17:09:04 +0900

    chore

--- src/views/live/live/LiveSession.vue
+++ src/views/live/live/LiveSession.vue
@@ -160,8 +160,8 @@ export default {
     methods: {
         async joinSession(sessionId) {
             console.log("화면들어옴 sessionId: ", sessionId);
-            this.OV = new OpenVidu("https://api.inong.shop:4443/");
-            // this.OV = new OpenVidu();
+            // this.OV = new OpenVidu("https://api.inong.shop:4443/");
+            this.OV = new OpenVidu();
             console.log(">>>>>새로운 openvidu객체: ", this.OV);
             this.session = this.OV.initSession();
             console.log(">>>>>initsession: ", this.session);
